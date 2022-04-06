2ce89810db2cf0f6c7300acfdcf831feb7286233
Kevin Zheng <kevinzheng813@gmail.com>
2022-04-06 00:28:37 -0400

    Merge remote-tracking branch 'origin/notebook_rev1' into main

--- preprocessing.py
+++ preprocessing.py
@@ -1,30 +1,64 @@
-from re import X
-import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA, TruncatedSVD, FastICA
 
-features_to_drop = []
+# keep 10 features
+features_to_keep = {"value_over_replacement_player", "true_shooting_percentage",
+    "win_shares_per_48_minutes", "player_efficiency_rating", "usage_percentage", "offensive_box_plus_minus",
+    "three_point_attempt_rate", "free_throw_attempt_rate", "steal_percentage"}
 data_type = "advanced"
 
-d_reduction_type = "PCA"
+drop_features = "full"
+normalized = "None"
+dimensionality_reduction = "None"
 
 data = pd.read_csv(f"data/partially_processed/{data_type}_player_season_totals.csv")
 
-x, y = data.drop(["name", "is_all_star"], axis=1), data["is_all_star"]
-encoded = pd.get_dummies(x["positions"], prefix="POS_")
-x = pd.merge(left=x.drop("positions", axis=1), right=encoded, left_index=True, right_index=True)
+encoded = pd.get_dummies(data["positions"], prefix="POS")
+data = pd.merge(left=data.drop("positions", axis=1), right=encoded, left_index=True, right_index=True)
 
-x = x.drop(features_to_drop, axis=1)
+X = data.drop("name", axis=1)
+positions = ['POS_CENTER', 'POS_POINT GUARD', 'POS_POWER FORWARD', 'POS_SHOOTING GUARD', 'POS_SMALL FORWARD']
+years = X['season'].unique()
 
-x, y = x.values, y.values
 
-if d_reduction_type == "PCA":
-    x = PCA(n_components=4).fit_transform(x)
-elif d_reduction_type == "SVD":
-    x = TruncatedSVD(n_components=4).fit_transform(x)
-elif d_reduction_type == "ICA":
-    x = FastICA(n_components=4)
+if normalized == "normalized":
+    working_X = []
+    for pos in positions:
+        for year in years:
+            #get players for a particular year and find means and variances for every feature
+            #current = X.where(X[pos] == 1 & X['season'] == year)
 
-data = np.hstack((x,y))
+            current = X[(X[pos] == 1) & (X['season'] == year)]
+            current = current.drop(columns=['season'])
 
-np.savetxt(f"data/fully_preprocessed/{data_type}.csv", data, delimiter=",")
+            all_star_col = current['is_all_star']
+            current = current.drop(columns=['is_all_star'])        
+
+            position_cols = current[positions]
+            current = current.drop(columns=positions)
+
+            current = (current - current.mean())/current.std()
+            current[positions] = position_cols
+            current['is_all_star'] = all_star_col
+            working_X.append(current)
+    X = pd.concat(working_X)
+
+if drop_features == "reduced":
+    features_to_drop = list(set(X.keys()).difference(features_to_keep))
+    x, y, names = X.drop(features_to_drop, axis=1), data["is_all_star"], data["name"]
+elif normalized != "normalized":
+    x, y, names = X.drop(["season", "is_all_star"]+positions, axis=1), data["is_all_star"], data["name"]
+else:
+    x, y, names = X.drop(["is_all_star"], axis=1), data["is_all_star"], data["name"]
+
+n = 8
+if dimensionality_reduction == "PCA":
+    x = pd.DataFrame(PCA(n_components=n).fit_transform(x), columns=[f"pca_{i}" for i in range(0,n)])
+elif dimensionality_reduction == "SVD":
+    x = TruncatedSVD(n_components=n).fit_transform(x)
+elif dimensionality_reduction == "ICA":
+    x = FastICA(n_components=n)
+
+data = pd.concat([names, x, y], axis=1)
+
+data.to_csv(f"data/fully_processed/{drop_features}_{normalized}_{dimensionality_reduction}.csv", sep=",", index=False)
